7024e2f8b9bc9883a23d999f4c19ca7ef12a399d
AndrewWang <yanroo8444@gmail.com>
2018-01-24 10:12:26 +0800

    migration to openshift v3

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ var AppContainer = function () {
    */
   self.setupVariables = function () {
     //  Set the environment variables we need.
-    self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-    self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+    self.ipaddress = '0.0.0.0';
+    self.port = 8080;
 
     if (typeof self.ipaddress === "undefined") {
       //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
       //  allows us to run/test the app locally.
       console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-      self.ipaddress = "127.0.0.1";
+      self.ipaddress = "0.0.0.0";
     }
   };
 
@@ -109,7 +109,7 @@ var AppContainer = function () {
 
       return false;
     }
-
+    self.app = express();
     /**
      * Event listener for HTTP server "error" event.
      */
